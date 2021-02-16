661159c802d646581db4f8178d01519aa2abd8a5
Berthier <tberthier@la-providence.net>
2021-02-16 17:41:29 +0100

    Exo 2 API

--- JavaScript/API.js
+++ JavaScript/API.js
@@ -1,6 +1,16 @@
 function demonstration2() {
     var div = document.getElementById("Root");
-    div.addEventListener("mouseover",function() {
+    div.addEventListener("click",function() {
         this.style.background = "red";
+        this.innerHTML = "Rouge";
     }, true)
+}
+
+function demonstration2bis() {
+    var div = document.getElementsByName("Test");
+    var nbdiv = 1;
+    div.forEach(element => {
+        element.innerHTML = "Je suis la div"+nbdiv;
+        nbdiv+=1;
+    });
 }
